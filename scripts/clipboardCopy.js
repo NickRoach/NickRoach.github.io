7badbc5e189be2b7fd5e35974dd3a54851bdf4d3
NickRoach <njlroach@gmail.com>
2021-08-22 11:04:15 +1000

    hover and click tooltip behavior corrected

--- scripts/clipboardCopy.js
+++ scripts/clipboardCopy.js
@@ -1,11 +1,23 @@
+function hoverFunction() {
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.style.opacity = 1;
+    console.log("hover called");
+}
+
 function myFunction() {
     var copyText = document.getElementById("myInput");
     copyText.select();
     copyText.setSelectionRange(0, 99999);
     document.execCommand("copy");
-
     var tooltip = document.getElementById("myTooltip");
+    tooltip.style.opacity = 1;
     tooltip.innerHTML = "Copied";
+    setTimeout(function () {
+        tooltip.style.opacity = 0;
+        setTimeout(function () {
+            tooltip.innerHTML = "Copy to clipboard";
+        }, 500);
+    }, 1000);
 }
 
 function outFunc() {
