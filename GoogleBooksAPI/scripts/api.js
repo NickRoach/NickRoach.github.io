aeccfa66b2bf21a02bfe41f583747296f611d5f5
NickRoach <njlroach@gmail.com>
2021-08-24 18:04:40 +1000

    google books and morse code deleted, buttons for third eye added

--- GoogleBooksAPI/scripts/api.js
+++
@@ -1,8 +0,0 @@
-export const getBooks = async (searchString) => {
-    const responsePromise = fetch(
-        `https://www.googleapis.com/books/v1/volumes?q=${searchString}&maxResults=39`
-    );
-    const response = await responsePromise;
-    const jsonResponse = await response.json();
-    return jsonResponse.items;
-};
